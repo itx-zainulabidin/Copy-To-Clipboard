eae3fb077c86b5fa6e0abb4dc723ebb74b6a26d8
Zain <zainulabidin9500@gmail.com>
2022-10-28 16:10:52 +0500

    Some minor changes

--- app.js
+++ app.js
@@ -3,14 +3,9 @@ const inputText = document.querySelector(".input-text");
 const btn = document.querySelector(".btn");
 const hi = document.querySelector(".hi");
 btn.addEventListener("click", () => {
-    inputText.select();
-    document.execCommand("copy");
-    // console.log(document.execCommand());
-    window.getSelection().removeAllRanges();
+    navigator.clipboard.writeText(inputText.value);
     copyText.classList.add("active");
     setTimeout(() => {
         copyText.classList.remove("active");
-
-
     }, 2000);
 });
